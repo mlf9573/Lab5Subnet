579f5be19d3d3379b3f22bec9821a4b4a38ee116
mlf9573 <michael.frenk@nyu.edu>
2024-03-30 18:45:50 -0400

    Update subnet.py

--- subnet.py
+++ subnet.py
@@ -1,65 +1,65 @@
 #enter your unique code
-uniquecode = 
+uniquecode = 83913
 
 #problem 1
-p1Network = ''
-p1Broadcast = ''
-p1First = ''
-p1Last =  ''
+p1Network = '31.86.14.0'
+p1Broadcast = '31.86.14.255'
+p1First = '31.86.14.1'
+p1Last =  '31.86.14.254'
 
 #problem 2
-p2Network = ''
-p2Broadcast = ''
-p2First = ''
-p2Last = ''
+p2Network = '198.71.231.112'
+p2Broadcast = '198.71.231.127'
+p2First = '198.71.231.113'
+p2Last = '198.71.231.126'
 
 #problem 3
-p3Network = ''
-p3Broadcast = ''
-p3First = ''
-p3Last = ''
+p3Network = '109.56.193.128'
+p3Broadcast = '109.56.193.255'
+p3First = '109.56.193.129'
+p3Last = '109.56.193.254'
 
 #problem 4
-p4Network = ''
-p4Broadcast = ''
-p4First = ''
-p4Last = ''
+p4Network = '20.42.155.208'
+p4Broadcast = '20.42.155.215'
+p4First = '20.42.155.209'
+p4Last = '20.42.155.214'
 
 #problem 5
-p5Network = ''
-p5Broadcast = ''
-p5First = ''
-p5Last = ''
+p5Network = '187.27.117.192'
+p5Broadcast = '187.27.117.255'
+p5First = '187.27.117.193'
+p5Last = '187.27.117.254'
 
 #problem 6
-p6Network = ''
-p6Broadcast = ''
-p6First = ''
-p6Last =  ''
+p6Network = '98.12.78.0'
+p6Broadcast = '98.12.78.255'
+p6First = '98.12.78.1'
+p6Last =  '98.12.78.254'
 
 #problem 7
-p7Network = ''
-p7Broadcast = ''
-p7First = ''
-p7Last = ''
+p7Network = '9.253.40.64'
+p7Broadcast = '9.253.40.95'
+p7First = '9.253.40.65'
+p7Last = '9.253.40.94'
 
 #problem 8
-p8Network = ''
-p8Broadcast = ''
-p8First = ''
-p8Last = ''
+p8Network = '176.238.2.0'
+p8Broadcast = '176.238.2.127'
+p8First = '176.238.2.1'
+p8Last = '176.238.2.126'
 
 #problem 9
-p9Network = ''
-p9Broadcast = ''
-p9First = ''
-p9Last = ''
+p9Network = '87.223.220.160'
+p9Broadcast = '87.223.220.175'
+p9First = '87.223.220.161'
+p9Last = '87.223.220.174'
 
 #problem 10
-p10Network = ''
-p10Broadcast = ''
-p10First = ''
-p10Last = ''
+p10Network = '254.208.181.192'
+p10Broadcast = '254.208.181.255'
+p10First = '254.208.181.193'
+p10Last = '254.208.181.254'
 
 
 """
